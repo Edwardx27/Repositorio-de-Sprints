eba69d1a0a2328d5b2fcba43271bf27645ed8aa9
Edwardx27 <lopezedward136@gmail.com>
2021-11-21 13:39:49 -0500

    navegacion con drawer

--- app/src/main/java/com/example/proyectosprint/MainActivity.kt
+++ app/src/main/java/com/example/proyectosprint/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.proyectosprint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     private lateinit var edtUsername: EditText
@@ -12,10 +15,42 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setSupportActionBar(findViewById(R.id.my_toolbar))
+
         edtUsername = findViewById(R.id.edtUsername)
         edtPassword = findViewById(R.id.edtPassword)
     }
+//inovocacion del toolbar y sus items
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_de_toolbar, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.action_search -> {
+                Toast.makeText(this, R.string.text_action_search, Toast.LENGTH_LONG).show()
+                return true
+            }
+            R.id.action_settings -> {
+                Toast.makeText(this, R.string.text_action_settings, Toast.LENGTH_LONG).show()
+                return true
+            }
+            R.id.action_logout -> {
+                Toast.makeText(this, R.string.text_action_logout, Toast.LENGTH_LONG).show()
+                return true
+            }
+            R.id.action_more -> {
+                val intento = Intent(this, DrawerActivity::class.java)
+                startActivity(intento)
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+  //      return super.onOptionsItemSelected(item)
+    }
+
+//funcion del boton que lleva a registro de ventas
     fun onLogin(view: android.view.View) {
         if (edtUsername.text.toString().equals("123456") && edtPassword.text.toString().equals("123456")){
             val intento = Intent(this, Registro_ventas::class.java)
--- app/src/main/java/com/example/proyectosprint/Registro_ventas.kt
+++ app/src/main/java/com/example/proyectosprint/Registro_ventas.kt
@@ -2,10 +2,42 @@ package com.example.proyectosprint
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 
 class Registro_ventas : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro_ventas)
+        setSupportActionBar(findViewById(R.id.my_toolbar))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_de_toolbar, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.action_search -> {
+                Toast.makeText(this, R.string.text_action_search, Toast.LENGTH_LONG).show()
+                return true
+            }
+            R.id.action_settings -> {
+                Toast.makeText(this, R.string.text_action_settings, Toast.LENGTH_LONG).show()
+                return true
+            }
+            R.id.action_logout -> {
+                Toast.makeText(this, R.string.text_action_logout, Toast.LENGTH_LONG).show()
+                return true
+            }
+            R.id.action_more -> {
+                Toast.makeText(this, R.string.text_action_more, Toast.LENGTH_LONG).show()
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+        //      return super.onOptionsItemSelected(item)
     }
 }
